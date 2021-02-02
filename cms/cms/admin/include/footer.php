d9c585f8cba2b886ec62f296b9932f5801983458
Ankit Saxena <ankit.friend07@gmail.com>
2021-02-02 17:25:03 +0530

    up and Complte V1

--- cms/cms/admin/include/footer.php
+++
@@ -1,8 +0,0 @@
-	<div class="footer">
-		<div class="container">
-			 
-
-			<b class="copyright"> © <a href="http://ascenttechsolution.com/">Ascent Tech Solution </a> | Complaint Management System 2021 </b> All rights reserved.
-		</div>
-	</div>
-	
