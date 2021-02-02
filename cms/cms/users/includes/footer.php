396d8c405b9ce650011165e5a82382995e50b90c
Ankit Saxena <ankit.friend07@gmail.com>
2021-02-02 17:23:24 +0530

    update Changes

---
+++ cms/cms/users/includes/footer.php
@@ -0,0 +1,8 @@
+ <footer class="site-footer">
+          <div class="text-center">
+              2021 - ascenttechsolution.com
+              <a href="#" class="go-top">
+                  <i class="fa fa-angle-up"></i>
+              </a>
+          </div>
+      </footer>
